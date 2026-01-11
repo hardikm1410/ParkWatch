97db63fbff0c31f25306896b7a3866c851bb9a13
Hardik Maniyar <hardik.baps.ahd@gmail.com>
2026-01-11 10:27:50 +0000

    why only 3?????where is other 2

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,6 +5,8 @@ import { PlaceHolderImages } from './placeholder-images';
 const connaughtPlaceImage = PlaceHolderImages.find(p => p.id === 'downtown-garage')!;
 const indiranagarImage = PlaceHolderImages.find(p => p.id === 'indiranagar-metro')!;
 const mumbaiAirportImage = PlaceHolderImages.find(p => p.id === 'mumbai-airport')!;
+const expressAvenueImage = PlaceHolderImages.find(p => p.id === 'express-avenue-mall')!;
+const wankhedeStadiumImage = PlaceHolderImages.find(p => p.id === 'wankhede-stadium')!;
 
 
 export const parkingLocations: ParkingLocation[] = [
@@ -13,7 +15,7 @@ export const parkingLocations: ParkingLocation[] = [
     name: 'Connaught Place Parking',
     address: 'Inner Circle, New Delhi',
     totalSpots: 500,
-    occupiedSpots: 52,
+    occupiedSpots: 53,
     currentFee: 150,
     imageUrl: connaughtPlaceImage.imageUrl,
     imageHint: connaughtPlaceImage.imageHint,
@@ -24,7 +26,7 @@ export const parkingLocations: ParkingLocation[] = [
     name: 'Indiranagar Metro Lot',
     address: 'CMH Road, Bangalore',
     totalSpots: 200,
-    occupiedSpots: 53,
+    occupiedSpots: 48,
     currentFee: 100,
     imageUrl: indiranagarImage.imageUrl,
     imageHint: indiranagarImage.imageHint,
@@ -35,12 +37,34 @@ export const parkingLocations: ParkingLocation[] = [
     name: 'Mumbai Airport Parking',
     address: 'Chhatrapati Shivaji Maharaj Int\'l',
     totalSpots: 1500,
-    occupiedSpots: 200,
+    occupiedSpots: 197,
     currentFee: 250,
     imageUrl: mumbaiAirportImage.imageUrl,
     imageHint: mumbaiAirportImage.imageHint,
     ownerId: "dummy-owner-3",
   },
+  {
+    id: 'express-avenue-mall',
+    name: 'Express Avenue Mall',
+    address: 'Whites Road, Royapettah, Chennai',
+    totalSpots: 1000,
+    occupiedSpots: 299,
+    currentFee: 120,
+    imageUrl: expressAvenueImage.imageUrl,
+    imageHint: expressAvenueImage.imageHint,
+    ownerId: "dummy-owner-4",
+  },
+  {
+    id: 'wankhede-stadium-lot',
+    name: 'Wankhede Stadium Lot',
+    address: 'D Road, Churchgate, Mumbai',
+    totalSpots: 3000,
+    occupiedSpots: 1502,
+    currentFee: 300,
+    imageUrl: wankhedeStadiumImage.imageUrl,
+    imageHint: wankhedeStadiumImage.imageHint,
+    ownerId: "dummy-owner-5",
+  }
 ];
 
 const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
