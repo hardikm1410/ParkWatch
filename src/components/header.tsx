919b7eaf50b534b9fa849aa38a2c4b0f91e82af8
Hardik Maniyar <hardik.baps.ahd@gmail.com>
2026-01-11 10:10:27 +0000

    change the find parking title with user

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -29,7 +29,7 @@ export function Header() {
           >
             <Link href="/">
               <User className="mr-2 h-4 w-4" />
-              Find Parking
+              User
             </Link>
           </Button>
           <Button
